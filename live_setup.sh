e2f7db1c869d56662ee1ce9fc2b51f515850d6ce
xelor <xelor@ya.ru>
2019-11-13 10:11:46 -0500

    browser and gitlens added

--- live_setup.sh
+++ live_setup.sh
@@ -5,6 +5,7 @@ git config --global user.email user@user.org
 
 # curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o ./soft/Miniconda3-latest-Linux-x86_64.sh
 # curl https://az764295.vo.msecnd.net/stable/86405ea23e3937316009fc27c9361deee66ffbf5/code_1.40.0-1573064499_amd64.deb -o ./soft/vscode.deb
+# curl https://www.torproject.org/dist/torbrowser/9.0.1/tor-browser-linux64-9.0.1_en-US.tar.xz -o ./soft/tor-browser-linux64-9.0.1_en-US.tar.xz
 # python3 -m pip download -d ./soft/pip_downloads datatable pandas jupyterlab jupytext rope pylint autopep8 yapf pwgen tldr seaborn xlrd xlwt xlsxwriter openpyxl sklearn catboost lightgbm tensorflow xgboost
 
 export PATH=~/miniconda/bin/:~/.local/bin:$PATH
@@ -19,3 +20,6 @@ for file in ./soft/vscode_extensions/*.vsix; do code --install-extension $file; 
 # code --install-extension ms-python.python
 # code --install-extension msrvida.vscode-sanddance
 # code --install-extension pkief.material-icon-theme
+# code --install-extension eamodio.gitlens
+
+tar xfv ./soft/tor-browser-linux64*.tar.xz -C ~/Desktop/
