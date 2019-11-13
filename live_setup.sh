708f09ec4b75949803ae069f0c9f8e55c61cc554
xelor <xelor@ya.ru>
2019-11-13 09:55:06 -0500

    python set to miniconda version, every possible package can be installed from local drive

--- live_setup.sh
+++ live_setup.sh
@@ -3,18 +3,19 @@
 git config --global user.name user
 git config --global user.email user@user.org
 
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py --user
+# curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o ./soft/Miniconda3-latest-Linux-x86_64.sh
+# curl https://az764295.vo.msecnd.net/stable/86405ea23e3937316009fc27c9361deee66ffbf5/code_1.40.0-1573064499_amd64.deb -o ./soft/vscode.deb
+# python3 -m pip download -d ./soft/pip_downloads datatable pandas jupyterlab jupytext rope pylint autopep8 yapf pwgen tldr seaborn xlrd xlwt xlsxwriter openpyxl sklearn catboost lightgbm tensorflow xgboost
 
-python3 -m pip install --user -U datatable pandas jupyterlab jupytext rope pylint autopep8 yapf pwgen tldr seaborn xlrd xlwt xlsxwriter openpyxl
-# python3 -m pip install --user sklearn catboost lightgbm tensorflow xgboost
+export PATH=~/miniconda/bin/:~/.local/bin:$PATH
+echo "export PATH=~/miniconda/bin/:~/.local/bin:$PATH" >> ~/.bashrc
 
-echo "export PATH=~/.local/bin:$PATH" >> ~/.bashrc
+bash ./soft/Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
+python3 -m pip install --user --find-links ./soft/pip_downloads datatable pandas jupyterlab jupytext rope pylint autopep8 yapf pwgen tldr seaborn xlrd xlwt xlsxwriter openpyxl sklearn catboost lightgbm tensorflow xgboost
 
-curl https://az764295.vo.msecnd.net/stable/86405ea23e3937316009fc27c9361deee66ffbf5/code_1.40.0-1573064499_amd64.deb -o vscode.deb
-sudo dpkg -i vscode.deb
-code --install-extension vscodevim.vim
-code --install-extension ms-python.python
-code --install-extension msrvida.vscode-sanddance
-code --install-extension pkief.material-icon-theme
-
+sudo dpkg -i ./soft/vscode.deb
+for file in ./soft/vscode_extensions/*.vsix; do code --install-extension $file; done
+# code --install-extension vscodevim.vim
+# code --install-extension ms-python.python
+# code --install-extension msrvida.vscode-sanddance
+# code --install-extension pkief.material-icon-theme
