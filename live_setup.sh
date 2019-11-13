92323c5e2daa593b70fa2d3ffba2803273fb9213
polyape1 <petr.polyakov@novartis.com>
2019-11-13 16:12:43 +0300

    prophet and plotly_express added

--- live_setup.sh
+++ live_setup.sh
@@ -6,13 +6,13 @@ git config --global user.email user@user.org
 # curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o ./soft/Miniconda3-latest-Linux-x86_64.sh
 # curl https://az764295.vo.msecnd.net/stable/86405ea23e3937316009fc27c9361deee66ffbf5/code_1.40.0-1573064499_amd64.deb -o ./soft/vscode.deb
 # curl https://www.torproject.org/dist/torbrowser/9.0.1/tor-browser-linux64-9.0.1_en-US.tar.xz -o ./soft/tor-browser-linux64-9.0.1_en-US.tar.xz
-# python3 -m pip download -d ./soft/pip_downloads datatable pandas jupyterlab jupytext rope pylint autopep8 yapf pwgen tldr seaborn xlrd xlwt xlsxwriter openpyxl sklearn catboost lightgbm tensorflow xgboost
+# python3 -m pip download -d ./soft/pip_downloads datatable pandas jupyterlab jupytext rope pylint autopep8 yapf pwgen tldr seaborn xlrd xlwt xlsxwriter openpyxl sklearn catboost lightgbm tensorflow xgboost fbprophet plotly_express
 
 export PATH=~/miniconda/bin/:~/.local/bin:$PATH
 echo "export PATH=~/miniconda/bin/:~/.local/bin:$PATH" >> ~/.bashrc
 
 bash ./soft/Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
-python3 -m pip install --user --find-links ./soft/pip_downloads datatable pandas jupyterlab jupytext rope pylint autopep8 yapf pwgen tldr seaborn xlrd xlwt xlsxwriter openpyxl sklearn catboost lightgbm tensorflow xgboost
+python3 -m pip install --user --find-links ./soft/pip_downloads datatable pandas jupyterlab jupytext rope pylint autopep8 yapf pwgen tldr seaborn xlrd xlwt xlsxwriter openpyxl sklearn catboost lightgbm tensorflow xgboost fbprophet plotly_express
 
 sudo dpkg -i ./soft/vscode.deb
 for file in ./soft/vscode_extensions/*.vsix; do code --install-extension $file; done
