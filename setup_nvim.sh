4e9d563ad7e71ff8fab5bec6934dca3dd7834d01
XelorR <xelor@ya.ru>
2020-02-09 06:22:10 +0300

    wget changed to curl

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.config/nvim/colors
-wget -O ~/.config/nvim/init.vim https://raw.githubusercontent.com/XelorR/dotfiles/master/mini_init.vim
-wget -O ~/.config/nvim/colors/molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+curl -fLo ~/.config/nvim/init.vim --create-dirs https://raw.githubusercontent.com/XelorR/dotfiles/master/mini_init.vim
+curl -fLo ~/.config/nvim/colors/molokai.vim --create-dirs https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
 sed -i 's/desert/molokai/g' ~/.config/nvim/init.vim
 
