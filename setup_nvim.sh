7e5fbebe4ffa9c1c1d9c6d17dcc2f73c9e5e6471
XelorR <xelor@ya.ru>
2020-02-26 18:17:37 +0300

    now it copy both configs -- vims and nvims

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -4,3 +4,8 @@ curl -fLo ~/.config/nvim/init.vim --create-dirs https://raw.githubusercontent.co
 curl -fLo ~/.config/nvim/colors/molokai.vim --create-dirs https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
 sed -i 's/desert/molokai/g' ~/.config/nvim/init.vim
 
+curl -fLo ~/.vimrc https://raw.githubusercontent.com/XelorR/dotfiles/master/mini_vimrc
+mkdir -p ~/.vim/colors
+cp ~/.config/nvim/colors/molokai.vim ~/.vim/colors/
+sed -i 's/desert/molokai/g' ~/.vimrc
+
