44119fcd7971bb4f2e96601e37574d724dea0b3c
XelorR <xelor@ya.ru>
2020-02-26 18:45:53 +0000

    python syntax highlit added

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -2,9 +2,11 @@
 
 curl -fLo ~/.config/nvim/init.vim --create-dirs https://raw.githubusercontent.com/XelorR/dotfiles/master/mini_init.vim
 curl -fLo ~/.config/nvim/colors/molokai.vim --create-dirs https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+curl -fLo ~/.config/nvim/syntax/python.vim --create-dirs https://raw.githubusercontent.com/hdima/python-syntax/master/syntax/python.vim
 sed -i 's/desert/molokai/g' ~/.config/nvim/init.vim
 
 curl -fLo ~/.vimrc https://raw.githubusercontent.com/XelorR/dotfiles/master/mini_vimrc
+curl -fLo ~/.vim/syntax/python.vim --create-dirs https://raw.githubusercontent.com/hdima/python-syntax/master/syntax/python.vim
 mkdir -p ~/.vim/colors
 cp ~/.config/nvim/colors/molokai.vim ~/.vim/colors/
 sed -i 's/desert/molokai/g' ~/.vimrc
