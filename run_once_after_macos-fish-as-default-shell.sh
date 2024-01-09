b1696d44d461d7b6a95ee2c09bf295b23651d98f
Petr <petr@duck.com>
2024-01-09 07:18:57 +0300

    mac fish -- automating set it as default shell

--- run_once_after_macos-fish-as-default-shell.sh
+++ run_once_after_macos-fish-as-default-shell.sh
@@ -1,5 +1,12 @@
-#!/usr/bin/env sh
+#!{{ lookPath "sh" }}
 
 # 1. sudo nano /etc/shells
 # 2. add /usr/local/bin/fish to your list of shells
 # 3. chsh -s /usr/local/bin/fish
+
+# example
+# $ echo /usr/local/bin/fish | sudo tee -a /etc/shells
+# $ chsh -s /usr/local/bin/fish
+
+echo {{ lookPath "fish" }} | sudo tee -a /etc/shells
+chsh -s {{ lookPath "fish" }}
