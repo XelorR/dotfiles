e0abdbe4f83267e3edf3d05436dff2feb10cb31b
Petr <petr@duck.com>
2024-02-26 21:43:43 +0300

    fish hydro --- switched to my prefix_vars branch

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,10 @@
 if status is-interactive
     set fish_greeting
 
+    set --global hydro_prefix_beginning \n
+    set --global hydro_prefix_git " "
+    set --global hydro_prefix_duration "󰅐 "
+
     set --global hydro_color_prompt 94e2d5
     set --global hydro_color_error f38ba8
     set --global hydro_multiline true
