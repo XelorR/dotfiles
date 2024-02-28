a716d7a7140c701178b758c5796e4250de1985f9
Petr <petr@duck.com>
2024-02-28 09:43:23 +0300

    fish themed with catppuccin

--- scripts/run_once_after_install-fish-plugins.sh
+++ scripts/run_once_after_install-fish-plugins.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update'
+fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update && fish_config theme save "Catppuccin Mocha"'
