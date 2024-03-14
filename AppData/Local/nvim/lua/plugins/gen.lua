2a5eeafcfb9dfe2f408696612b7105723e0fb022
Petr <petr@duck.com>
2024-03-14 21:00:33 +0300

    gen.nvim --- kemap added + floating changed to split

--- AppData/Local/nvim/lua/plugins/gen.lua
+++ AppData/Local/nvim/lua/plugins/gen.lua
@@ -4,7 +4,7 @@ return {
     model = "openhermes", -- The default model to use.
     host = "localhost", -- The host running the Ollama service.
     port = "11434", -- The port on which the Ollama service is listening.
-    display_mode = "float", -- The display mode. Can be "float" or "split".
+    display_mode = "split", -- The display mode. Can be "float" or "split".
     show_prompt = false, -- Shows the Prompt submitted to Ollama.
     show_model = false, -- Displays which model you are using at the beginning of your chat session.
     quit_map = "q", -- set keymap for quit
