f793edd369db8cc0b10b4631263d7aae9b1a29d2
Petr <petr@duck.com>
2024-04-13 18:33:44 +0300

    macos --- more speedup optimizations

--- scripts/macos/run_once_before_macos-instantly-show-dock-on-mouse-hover.sh
+++ scripts/macos/run_once_before_macos-instantly-show-dock-on-mouse-hover.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env zsh
 
 defaults write com.apple.dock "autohide-delay" -float "0" && killall Dock
+defaults write com.apple.dock launchanim -bool false
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+defaults write -g QLPanelAnimationDuration -float 0
+defaults write -g NSWindowResizeTime -float 0.001
+
