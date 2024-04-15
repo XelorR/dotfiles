5debf107a13f051bfc5dc5dbd32f57ffb6b12a5f
Petr <petr@duck.com>
2024-04-15 10:47:12 +0300

    tap emacs-mac --- switched to cask

--- scripts/extras/tap/emacs-mac.sh
+++ scripts/extras/tap/emacs-mac.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
 brew tap railwaycat/emacsmacport
-brew install emacs-mac --with-modules
-ln -s /usr/local/opt/emacs-mac/Emacs.app /Applications/Emacs.app
+brew install --cask emacs-mac # --with-modules
+# ln -s /usr/local/opt/emacs-mac/Emacs.app /Applications/Emacs.app
+
