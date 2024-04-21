ea56ad9cd178aaa488b7216df11ffc19d38763f3
Petr <petr@duck.com>
2024-04-21 10:03:31 +0300

    fish installer --- conditions for debian and ubuntu added

--- scripts/extras/apt/fish.sh
+++ scripts/extras/apt/fish.sh
@@ -1,9 +1,29 @@
 #!/usr/bin/env sh
 
-# Debian Bookworm version
-# TODO - conditionally select disro basing on /etc/os-release
+. /etc/os-release
 
-echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
-curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
-sudo apt update
-sudo apt install -y fish
+if [ "$ID" = "debian" ]; then
+    if [ "VERSION_ID" = "12" ]; then
+        echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
+        curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+    elif [ "VERSION_ID" = "11" ]; then
+        echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
+        curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+    elif [ "VERSION_ID" = "10" ]; then
+        echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_10/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
+        curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+    fi
+    sudo apt update
+    sudo apt install -y fish
+elif [ "$ID" = "ubuntu" ]; then
+    if [ "VERSION_ID" = "24.04" ]; then
+        sudo apt update
+        sudo apt install -y fish
+    else
+        sudo apt-add-repository ppa:fish-shell/release-3
+        sudo apt update
+        sudo apt install -y fish
+    fi
+else
+    echo This script is for Ubuntu and Debian only.
+fi
