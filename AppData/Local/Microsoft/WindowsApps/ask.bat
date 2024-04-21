879fd9e3e4a63c162417394c059eea1cbfad282f
Petr <petr@duck.com>
2024-04-21 09:31:46 +0300

    ask and coder on windows -- added the --help option

--- AppData/Local/Microsoft/WindowsApps/ask.bat
+++ AppData/Local/Microsoft/WindowsApps/ask.bat
@@ -1,3 +1,8 @@
 @echo off
 
+if "%~1" == "--help" (
+    echo Usage: ask [your question...]
+    exit /b 0
+)
+
 ollama run nous-hermes2 %*
--- AppData/Local/Microsoft/WindowsApps/coder.bat
+++ AppData/Local/Microsoft/WindowsApps/coder.bat
@@ -1,3 +1,8 @@
 @echo off
 
+if "%~1" == "--help" (
+    echo Usage: coder [your question...]
+    exit /b 0
+)
+
 ollama run codeqwen:chat %*
