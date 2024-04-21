7af2299aaca6f9e661e043511864147d8e3f4fa2
Petr <petr@duck.com>
2024-04-21 18:52:16 +0300

    lazyvim - default gen model set to codeqwen:chat

--- AppData/Local/nvim/lua/plugins/gen.lua
+++ AppData/Local/nvim/lua/plugins/gen.lua
@@ -1,7 +1,7 @@
 return {
   "David-Kunz/gen.nvim",
   opts = {
-    model = "openhermes", -- The default model to use.
+    model = "codeqwen:chat", -- The default model to use.
     host = "localhost", -- The host running the Ollama service.
     port = "11434", -- The port on which the Ollama service is listening.
     display_mode = "float", -- The display mode. Can be "float" or "split".
