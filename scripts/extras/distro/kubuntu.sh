c426f1a76fd90ed5e05522da968bd74da1045aa6
Petr <petr@duck.com>
2024-04-24 01:45:47 +0300

    distro packages --- kubuntu install added

---
+++ scripts/extras/distro/kubuntu.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt install -y \
+	aria2 \
+	bat \
+	borgbackup \
+	curl \
+	docker{.io,-compose} \
+	eza \
+	fd-find \
+	ffmpeg \
+	flatpak \
+	fzf \
+	gcc \
+	git \
+	golang \
+	heif-{gdk-pixbuf,thumbnailer} \
+	imagemagick \
+	jq \
+	keepassxc \
+	luajit \
+	make \
+	ncdu \
+	npm \
+	p7zip-full \
+	python3-{pip,llfuse} \
+	qalc \
+	qbittorrent \
+	qemu-kvm \
+	ripgrep \
+	rsync \
+	tmate \
+	tmux \
+	virt-manager \
+	vlc \
+	vorta \
+	wget
