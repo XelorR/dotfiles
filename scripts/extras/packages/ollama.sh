74c3dfa830c6d9816e9cae82439ec22bfa8681c6
Petr <petr@duck.com>
2024-05-04 15:34:20 +0300

    ollama installation fixed

--- scripts/extras/packages/ollama.sh
+++ scripts/extras/packages/ollama.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env sh
 
 # Define Ollama installation path
+mkdir -p $HOME/.local/bin
 OLLAMA_PATH="$HOME/.local/bin/ollama"
 
 # Download and install Ollama
 wget https://ollama.com/download/ollama-linux-amd64 -O $OLLAMA_PATH && chmod +x $OLLAMA_PATH
 
 # Create systemd service file for Ollama
+mkdir -p $HOME/.config/systemd/user
 cat << EOF > $HOME/.config/systemd/user/ollama.service
 [Unit]
 Description=Ollama Service
