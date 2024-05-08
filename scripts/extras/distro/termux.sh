6f27267a374f5eac70dcb0ebccfe31154d223052
Petr <petr@duck.com>
2024-05-08 20:01:17 +0300

    termux package installer added

---
+++ scripts/extras/distro/termux.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+apt install -y \
+	aria2 \
+	bat \
+	calc \
+	chezmoi \
+	clang \
+	cmake \
+	emacs \
+	eza \
+	fd \
+	fish \
+	fzf \
+	lazygit \
+	lf \
+	luajit \
+	make \
+	neovim \
+	nmap \
+	nodejs \
+	openssh \
+	p7zip \
+	python \
+	ripgrep \
+	rsync \
+	tmate \
+	tmux \
+	w3m \
+	zsh
+
+chsh -s zsh
