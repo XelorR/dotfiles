1279ffbddd05028ce74fa4c8554ca949f713029e
Petr <petr@duck.com>
2024-05-12 02:58:01 +0300

    anlinux arch packages added

---
+++ scripts/extras/distro/termux-anlinux-arch.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+pacman -Syu \
+	aria2 \
+	bat \
+	borgbackup \
+	cdrkit \
+	chezmoi \
+	emacs \
+	eza \
+	fd \
+	ffmpeg \
+	fzf \
+	gcc \
+	git \
+	github-cli \
+	go \
+	imagemagick \
+	jq \
+	lazygit \
+	lf \
+	luajit \
+	make \
+	neovim \
+	npm \
+	python-llfuse \
+	qalculate-qt \
+	ripgrep \
+	rsync \
+	tmate \
+	tmux \
+	vlc \
+	w3m \
+	zsh
