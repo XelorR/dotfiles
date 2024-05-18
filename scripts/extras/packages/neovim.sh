259630f6c626114abfaa4842fc037fca899367f0
Petr <petr@duck.com>
2024-05-18 16:37:36 +0300

    neovim installation - typo fixed

--- scripts/extras/packages/neovim.sh
+++ scripts/extras/packages/neovim.sh
@@ -3,7 +3,7 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 
 mkdir -p $HOME/.opt $HOME/.local/bin
-rm -rf $HOME/.opt/nvim
+rm -rf $HOME/.opt/nvim-linux64
 
 tar -C $HOME/.opt -xzf nvim-linux64.tar.gz
 rm -rf nvim-linux64.tar.gz
