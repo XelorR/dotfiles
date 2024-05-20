4f3508e7b5353390b3e3ef4b4f7287b42f4b5d1c
Petr <petr@duck.com>
2024-05-20 13:39:43 +0300

    vscode extensions installation sctipt saved

---
+++ scripts/extras/vscode/install-extensions.bat
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# vscode extensions
+
+code --install-extension compulim.vscode-qrcodecode --install-extension continue.continue
+code --install-extension davidanson.vscode-markdownlintcode --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlenscode --install-extension esbenp.prettier-vscode
+code --install-extension github.remotehubcode --install-extension grapecity.gc-excelviewer
+code --install-extension hediet.vscode-drawiocode --install-extension ionide.ionide-fsharp
+code --install-extension ms-dotnettools.csharpcode --install-extension ms-dotnettools.dotnet-interactive-vscode
+code --install-extension ms-dotnettools.vscode-dotnet-packcode --install-extension ms-dotnettools.vscode-dotnet-runtime
+code --install-extension ms-python.debugpycode --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylancecode --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymapcode --install-extension ms-toolsai.jupyter-renderers
+code --install-extension ms-toolsai.vscode-jupyter-cell-tagscode --install-extension ms-toolsai.vscode-jupyter-slideshow
+code --install-extension ms-vscode-remote.remote-containerscode --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-editcode --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode-remote.vscode-remote-extensionpackcode --install-extension ms-vscode.azure-repos
+code --install-extension ms-vscode.cpptoolscode --install-extension ms-vscode.live-server
+code --install-extension ms-vscode.powershellcode --install-extension ms-vscode.remote-explorer
+code --install-extension ms-vscode.remote-repositoriescode --install-extension ms-vscode.remote-server
+code --install-extension ms-vsliveshare.vslivesharecode --install-extension nilssoderman.batch-runner
+code --install-extension pomdtr.excalidraw-editorcode --install-extension redhat.java
+code --install-extension shd101wyy.markdown-preview-enhancedcode --install-extension theumletteam.umlet
+code --install-extension tomoki1207.pdfcode --install-extension visualstudioexptteam.intellicode-api-usage-examples
+code --install-extension visualstudioexptteam.vscodeintellicode
+
