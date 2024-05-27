5b07829d6981b32a3076d48566a9ed23c49d4ed5
Petr <petr@duck.com>
2024-05-27 09:57:31 +0300

    gnome script enchanced

--- scripts/linux/run_once_gnome-assign-shortcuts.sh
+++ scripts/linux/run_once_gnome-assign-shortcuts.sh
@@ -2,24 +2,31 @@
 
 # gsettings list-recursively org.gnome.desktop.wm.keybindings | grep to-workspace
 
-if [ "$XDG_CURRENT_DESKTOP" == "GNOME" ]; then
+if [[ "$XDG_CURRENT_DESKTOP" == "GNOME" || "$XDG_CURRENT_DESKTOP" == "ubuntu:GNOME" ]]; then
+	echo GNOME detected!
 	if command -v gsettings &>/dev/null; then
+		echo gsettings available!
 		gsettings set org.gnome.mutter dynamic-workspaces false
 		gsettings set org.gnome.desktop.wm.preferences num-workspaces 10
 
 		# keyboard shortcuts
 		for i in $(seq 9); do
-			gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-$i "['<Control><Super><Shift>$i']"
-			gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-$i "['<Control><Super>$i']"
+			echo Configuring desktop $i...
+			gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-$i "['<Control><Alt><Shift>$i']"
+			gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-$i "['<Control><Alt>$i']"
 		done
-		gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10 "['<Control><Super><Shift>0']"
-		gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Control><Super>0']"
+		echo Configuring desktop 10...
+		gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10 "['<Control><Alt><Shift>0']"
+		gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Control><Alt>0']"
 
+		echo Configuring Alt-tab and Super-tab...
 		gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab']"
 		gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Shift><Super>Tab']"
 		gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
 		gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
 
+		echo Configuring Super-Shift-s ta make a screenshot...
 		gsettings set org.gnome.shell.keybindings show-screenshot-ui "['Print', '<Shift><Super>s']"
+		echo Done!
 	fi
 fi
