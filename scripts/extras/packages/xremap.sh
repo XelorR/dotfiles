7c7fdafee37fd16363b3ddb6b7aeee19227d179a
Petr <petr@duck.com>
2024-05-27 11:25:27 +0300

    xremap installer gnome option added

--- scripts/extras/packages/xremap.sh
+++ scripts/extras/packages/xremap.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-xremap_url=$(curl -sL https://api.github.com/repos/k0kubun/xremap/releases/latest | grep x86_64-x11.zip | grep browser | cut -d'"' -f4)
+if [[ "$XDG_CURRENT_DESKTOP" == "GNOME" || "$XDG_CURRENT_DESKTOP" == "ubuntu:GNOME" ]]; then
+	platform="gnome"
+else
+	platform="x11"
+fi
+xremap_url=$(curl -sL https://api.github.com/repos/k0kubun/xremap/releases/latest | grep x86_64-$platform.zip | grep browser | cut -d'"' -f4)
 xremap_name=$(echo $xremap_url | cut -d'/' -f9)
 
 wget $xremap_url
 
-mkdir -p ~/.local/bin/
-7z x $xremap_name -O./.local/bin/
+mkdir -p $HOME/.local/bin/
+7z x $xremap_name -O$HOME/.local/bin/
 rm $xremap_name
 
 sudo gpasswd -a $USER input
