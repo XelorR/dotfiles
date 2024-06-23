536445743daf09430f9b825303a0ca5338e2790d
Petr <petr@duck.com>
2024-06-23 13:41:09 +0300

    heeedoc corrected

--- scripts/extras/packages/prezto.sh
+++ scripts/extras/packages/prezto.sh
@@ -13,12 +13,12 @@ elif [ "$ID_LIKE" = "arch" ]; then
 	sudo pacman -Sy zsh git
 fi
 
-zsh -c 'cat <<EOF
+zsh <<'EOF'
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
-EOF'
+EOF
 
 sudo sed -i "s|^\($USER.*\)/bin/bash|\1/bin/zsh|" /etc/passwd
