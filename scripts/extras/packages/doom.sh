3e36d9e9bb70198c70a4cf424e64aa1e4356de91
Petr <petr@duck.com>
2024-07-18 03:12:11 +0300

    clean doom install

--- scripts/extras/packages/doom.sh
+++ scripts/extras/packages/doom.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+rm -rf ~/.opt/doom
 mkdir -p ~/.opt/doom
 env HOME=$HOME/.opt/doom git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
 env HOME=$HOME/.opt/doom ~/.opt/doom/.config/emacs/bin/doom install --env -! --emacsdir ~/.config/emacs
