e2c247fc23763d1ad6d4401966780dd5053ea2c7
Petr <petr@duck.com>
2024-07-18 03:14:22 +0300

    path fix

--- scripts/extras/packages/doom.sh
+++ scripts/extras/packages/doom.sh
@@ -3,7 +3,7 @@
 rm -rf ~/.opt/doom
 mkdir -p ~/.opt/doom
 env HOME=$HOME/.opt/doom git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
-env HOME=$HOME/.opt/doom ~/.opt/doom/.config/emacs/bin/doom install --env -! --emacsdir ~/.config/emacs
+env HOME=$HOME/.opt/doom ~/.config/emacs/bin/doom install --env -! --emacsdir ~/.config/emacs
 
 # then you can run Doom Emacs
 # without affecting your configuration,
