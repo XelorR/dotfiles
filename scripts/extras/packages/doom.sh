1fdd5ef954fccba1a963064843d13f8bb0b31219
Petr <petr@duck.com>
2024-07-18 20:36:20 +0300

    doom installer fixed

--- scripts/extras/packages/doom.sh
+++ scripts/extras/packages/doom.sh
@@ -2,10 +2,9 @@
 
 rm -rf ~/.opt/doom
 mkdir -p ~/.opt/doom
-env HOME=$HOME/.opt/doom git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
-env HOME=$HOME/.opt/doom ~/.config/emacs/bin/doom install --env -! --emacsdir ~/.config/emacs
+( HOME=$HOME/.opt/doom; git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs )
+( HOME=$HOME/.opt/doom; ~/.config/emacs/bin/doom install --env -! )
 
-# then you can run Doom Emacs
-# without affecting your configuration,
-# using command like this:
-# env HOME=$HOME/.opt/doom emacs &!
+echo Done!
+echo Now you can run Doom Emacs using the following command:
+echo '( HOME=$HOME/.opt/doom; emacs &! )'
