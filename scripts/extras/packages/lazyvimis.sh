4b426c9432c7165d8427c00512103cbd60441901
Petr <petr@duck.com>
2024-07-27 20:13:00 +0300

    lazyvim separated installation added

---
+++ scripts/extras/packages/lazyvimis.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+if ! command -v nvim &>/dev/null; then
+  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+
+  mkdir -p $HOME/.opt $HOME/.local/bin
+  rm -rf $HOME/.opt/nvim-linux64
+
+  tar -C $HOME/.opt -xzf nvim-linux64.tar.gz
+  rm -rf nvim-linux64.tar.gz
+
+  rm -rf $HOME/.local/bin/nvim
+  ln -s $HOME/.opt/nvim-linux64/bin/nvim $HOME/.local/bin/nvim
+
+  if ! grep -q '.local/bin' $HOME/.bashrc; then
+    echo 'export PATH=$HOME/.local/bin:$PATH' >>$HOME/.bashrc
+  fi
+  if ! grep -q '.local/bin' $HOME/.zshrc; then
+    echo 'export PATH=$HOME/.local/bin:$PATH' >>$HOME/.zshrc
+  fi
+fi
+
+rm -rf ~/.opt/lazyvim
+mkdir -p ~/.opt/lazyvim ~/.local/bin
+
+cat <<'EOF' >~/.local/bin/lazyvim
+#!/usr/bin/env bash
+
+( HOME=$HOME/.opt/lazyvim; nvim $@ )
+EOF
+chmod +x ~/.local/bin/lazyvim
+
+(
+  HOME=$HOME/.opt/neovim
+
+  git clone https://github.com/LazyVim/starter ~/.config/nvim
+  rm -rf ~/.config/nvim/.git
+
+  mkdir -p ~/.config/nvim/lua/plugins
+  cat <<'EOF' >~/.config/nvim/lua/plugins/nvim-notify.lua
+return {
+    "rcarriga/nvim-notify",
+    opts = {
+        level = 5,
+        render = "compact",
+        stages = "static",
+    },
+}
+EOF
+
+)
