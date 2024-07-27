97d3eb7bb69edcbeaa6daa950d47a520ee4152aa
Petr <petr@duck.com>
2024-07-27 18:28:20 +0300

    vhezmoi nvim added

--- AppData/Local/nvim/lua/config/lazy.lua
+++ AppData/Local/nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.dap.core" },
 		{ import = "lazyvim.plugins.extras.vscode" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.util.chezmoi" },
 		-- { import = "lazyvim.plugins.extras.ui.mini-animate" },
 		-- import/override with your plugins
 		{ import = "plugins" },
