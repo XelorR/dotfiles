4c2425b43ef5187b82bcd8ba52e57a1559aae733
Petr <petr@duck.com>
2024-07-30 09:21:41 +0300

    extracted variable

--- scripts/extras/packages/ventoy.sh
+++ scripts/extras/packages/ventoy.sh
@@ -3,11 +3,12 @@
 # get link
 VERSION="$(curl -sL https://github.com/ventoy/Ventoy/releases | grep -E 'Ventoy .* release' | cut -d ' ' -f 6 | head -n 1)"
 FILE="ventoy-"$VERSION"-linux.tar.gz"
+FOLDER="$HOME/.opt"
 URL="https://github.com/ventoy/Ventoy/releases/download/v"$VERSION"/$FILE"
 
 # download
-rm -rf "$HOME/.opt/$FILE"
-mkdir -p "$HOME/.opt"
-curl --http1.1 "$URL" --output-dir "$HOME/.opt" --output "$FILE"
+rm -rf "$FOLDER/$FILE"
+mkdir -p "$FOLDER"
+curl --http1.1 "$URL" --output-dir "$FOLDER" --output "$FILE"
 
 # unpack
