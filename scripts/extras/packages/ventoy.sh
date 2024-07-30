bbb34167f6d448e83f6fec41791cfeb9e63fe2c4
Petr <petr@duck.com>
2024-07-30 09:14:44 +0300

    download added

--- scripts/extras/packages/ventoy.sh
+++ scripts/extras/packages/ventoy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# get link
 VERSION="$(curl -sL https://github.com/ventoy/Ventoy/releases | grep -E 'Ventoy .* release' | cut -d ' ' -f 6 | head -n 1)"
 URL="https://github.com/ventoy/Ventoy/releases/download/v"$VERSION"/ventoy-"$VERSION"-linux.tar.gz"
+FILE="ventoy-"$VERSION"-linux.tar.gz"
 
+# download
+rm -rf "$FILE"
 mkdir -p ~/.opt
-echo $URL
+curl --http1.1 "$URL" --output-dir "$HOME/.opt" --output "$FILE"
+
+# unpack
