f3663584dcf240fd98bb64f6ef0874ecdb9ffe6d
Petr <petr@duck.com>
2024-07-30 18:36:15 +0300

    reformatted + visual mode command launching

--- dot_config/nvchad/lua/mappings.lua
+++ dot_config/nvchad/lua/mappings.lua
@@ -1,4 +1,4 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
@@ -26,27 +26,27 @@ map("n", "<leader>bb", "<cmd>Telescope buffers<CR>", { desc = "telescope find bu
 map("n", "<leader>bo", "<cmd>%bd|e#|bd#<cr>", { noremap = true, desc = "delete other buffers" })
 
 map("n", "L", function()
-  require("nvchad.tabufline").next()
+	require("nvchad.tabufline").next()
 end, { desc = "buffer goto next" })
 
 map("n", "H", function()
-  require("nvchad.tabufline").prev()
+	require("nvchad.tabufline").prev()
 end, { desc = "buffer goto prev" })
 
 map("n", "<leader>bn", function()
-  require("nvchad.tabufline").next()
+	require("nvchad.tabufline").next()
 end, { desc = "buffer goto next" })
 
 map("n", "<leader>bp", function()
-  require("nvchad.tabufline").prev()
+	require("nvchad.tabufline").prev()
 end, { desc = "buffer goto prev" })
 
 map("n", "<leader>bd", function()
-  require("nvchad.tabufline").close_buffer()
+	require("nvchad.tabufline").close_buffer()
 end, { desc = "buffer close" })
 
 map("n", "<leader>bk", function()
-  require("nvchad.tabufline").close_buffer()
+	require("nvchad.tabufline").close_buffer()
 end, { desc = "buffer close" })
 
 -- windows
@@ -58,26 +58,26 @@ map("n", "<leader>wo", "<C-w>o", { desc = "close other windows" })
 map("n", "<leader>ws", "<C-w>s", { desc = "split window horisontally" })
 map("n", "<leader>wh", "<C-w>s", { desc = "split window horisontally" })
 map("n", "<leader>wv", "<C-w>v", { desc = "split window vertically" })
-map("n", "<C-left>", "<cmd>vert res -5<cr>" )
-map("n", "<C-right>", "<cmd>vert res +5<cr>" )
-map("n", "<C-down>", "<cmd>res -5<cr>" )
-map("n", "<C-up>", "<cmd>res +5<cr>" )
+map("n", "<C-left>", "<cmd>vert res -5<cr>")
+map("n", "<C-right>", "<cmd>vert res +5<cr>")
+map("n", "<C-down>", "<cmd>res -5<cr>")
+map("n", "<C-up>", "<cmd>res +5<cr>")
 map({ "n", "v" }, "<C-h>", "<C-w>h", { desc = "switch window left" })
 map({ "n", "v" }, "<C-l>", "<C-w>l", { desc = "switch window right" })
 map({ "n", "v" }, "<C-j>", "<C-w>j", { desc = "switch window down" })
 map({ "n", "v" }, "<C-k>", "<C-w>k", { desc = "switch window up" })
-map( "i", "<C-h>", "<esc><C-w>h", { desc = "switch window left" })
-map( "i", "<C-l>", "<esc><C-w>l", { desc = "switch window right" })
-map( "i", "<C-j>", "<esc><C-w>j", { desc = "switch window down" })
-map( "i", "<C-k>", "<esc><C-w>k", { desc = "switch window up" })
-map( "t", "<C-h>", "<C-\\><C-n><C-w>h", { desc = "switch window left" })
-map( "t", "<C-l>", "<C-\\><C-n><C-w>l", { desc = "switch window right" })
-map( "t", "<C-j>", "<C-\\><C-n><C-w>j", { desc = "switch window down" })
-map( "t", "<C-k>", "<C-\\><C-n><C-w>k", { desc = "switch window up" })
+map("i", "<C-h>", "<esc><C-w>h", { desc = "switch window left" })
+map("i", "<C-l>", "<esc><C-w>l", { desc = "switch window right" })
+map("i", "<C-j>", "<esc><C-w>j", { desc = "switch window down" })
+map("i", "<C-k>", "<esc><C-w>k", { desc = "switch window up" })
+map("t", "<C-h>", "<C-\\><C-n><C-w>h", { desc = "switch window left" })
+map("t", "<C-l>", "<C-\\><C-n><C-w>l", { desc = "switch window right" })
+map("t", "<C-j>", "<C-\\><C-n><C-w>j", { desc = "switch window down" })
+map("t", "<C-k>", "<C-\\><C-n><C-w>k", { desc = "switch window up" })
 
 -- run command
 map("i", "<M-x>", "<Esc>:")
-map("n", "<M-x>", ":")
+map({ "n", "v" }, "<M-x>", ":")
 
 -- overridings
 map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
