c48d5ff7ede28f6de11bca54aa5eb08aeea8245d
Petr <petr@duck.com>
2024-07-30 08:57:47 +0300

    url generated, folder created

--- scripts/extras/packages/ventoy.sh
+++ scripts/extras/packages/ventoy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 VERSION="$(curl -sL https://github.com/ventoy/Ventoy/releases | grep -E 'Ventoy .* release' | cut -d ' ' -f 6 | head -n 1)"
+URL="https://github.com/ventoy/Ventoy/releases/download/v"$VERSION"/ventoy-"$VERSION"-linux.tar.gz"
 
-echo $VERSION
+mkdir -p ~/.opt
+echo $URL
