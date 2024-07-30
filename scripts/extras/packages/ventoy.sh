18aafd899368ca6b7485388ba5fc9783a7cb7d47
Petr <petr@duck.com>
2024-07-30 09:30:09 +0300

    extraction initiated
    but doesnt work at this moment

--- scripts/extras/packages/ventoy.sh
+++ scripts/extras/packages/ventoy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# get link
+# get link and set variables
 VERSION="$(curl -sL https://github.com/ventoy/Ventoy/releases | grep -E 'Ventoy .* release' | cut -d ' ' -f 6 | head -n 1)"
 FILE="ventoy-"$VERSION"-linux.tar.gz"
 FOLDER="$HOME/.opt"
 URL="https://github.com/ventoy/Ventoy/releases/download/v"$VERSION"/$FILE"
+EXTRACT_FOLDER="$(echo $FILE | sed 's/.tar.gz$//')"
 
 # download
 rm -rf "$FOLDER/$FILE"
@@ -12,3 +13,4 @@ mkdir -p "$FOLDER"
 curl --http1.1 "$URL" --output-dir "$FOLDER" --output "$FILE"
 
 # unpack
+tar xzf "$FOLDER/$FILE" -C "$FOLDER/$EXTRACT_FOLDER"
