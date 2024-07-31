45eefb80203f4295bae2b24f2c3321940c5f90cc
Petr <petr@duck.com>
2024-07-31 10:10:54 +0300

    treesitter enabled

--- scripts/extras/packages/nvchad.sh
+++ scripts/extras/packages/nvchad.sh
@@ -222,3 +222,30 @@ local options = {
 
 return options
 EOF
+
+cat <<'EOF' >~/.config/nvchad/lua/plugins/init.lua
+return {
+	{
+		"stevearc/conform.nvim",
+		-- event = "BufWritePre", -- uncomment for format on save
+		opts = require("configs.conform"),
+	},
+
+	-- These are some examples, uncomment them if you want to see them work!
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"vim", "lua", "vimdoc",
+	     "html", "css", "python",
+			},
+		},
+	},
+EOF
