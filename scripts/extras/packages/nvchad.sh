4d0cddb2a1fb24ed7d9f609bc74576f109f2e113
Petr <petr@duck.com>
2024-07-31 19:01:32 +0300

    marksman added

--- scripts/extras/packages/nvchad.sh
+++ scripts/extras/packages/nvchad.sh
@@ -184,7 +184,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "pyright", "bashls" }
+local servers = { "html", "cssls", "pyright", "bashls", "marksman" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
