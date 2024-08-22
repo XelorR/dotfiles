95d8a1ceacb80ce10b91cfb85f595041c4aeccb2
Petr <petr@duck.com>
2024-08-22 12:11:30 +0300

    endeavouros setup added

---
+++ scripts/extras/distro/endeavouros-gnome.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/env bash
+
+# assuming gnome version
+# enabling ubuntu-like alt-tab and windows-like win-r
+echo Configuring Alt-tab, Super-grave and Super-tab...
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Shift><Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-group "['<Super>Above_Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-group-backward "['<Shift><Super>Above_Tab']"
+
+echo Configuring Win-r...
+gsettings set org.gnome.desktop.wm.keybindings panel-run-dialog "['<Alt>F1', '<Super>r']"
+
+echo Configuring scrolling...
+gsettings set org.gnome.desktop.peripherals.mouse natural-scroll false
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll true
+
+echo Configuring Super-Shift-s to make a screenshot...
+gsettings set org.gnome.shell.keybindings show-screenshot-ui "['Print', '<Shift><Super>s']"
+
+# enabling cachy os repos
+curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
+tar xvf cachyos-repo.tar.xz && cd cachyos-repo
+sudo ./cachyos-repo.sh
+
+# installing packages
+sudo pacman -Syu --noconfirm \
+	aria2 \
+	bash-language-server \
+	chezmoi \
+	curl \
+	docker \
+	docker-compose \
+	element-desktop \
+	emacs \
+	fd \
+	firefox \
+	flatpak \
+	fzf \
+	github-cli \
+	gnome-shell-extension-caffeine \
+	gnome-shell-extension-dash-to-dock \
+	gnome-shell-extensions \
+	gnome-tweaks \
+	helix \
+	jq \
+	keepassxc \
+	lazygit \
+	lf \
+	libreoffice-fresh \
+	marksman \
+	minetest \
+	minikube \
+	nautilus-image-converter \
+	ncdu \
+	neovim \
+	nmap \
+	npm \
+	ollama \
+	p7zip \
+	prettier \
+	proxychains \
+	pyright \
+	python-black \
+	python-pip \
+	qbittorrent \
+	qemu-desktop \
+	ripgrep \
+	rsync \
+	serpl \
+	sshfs \
+	sushi \
+	syncthing \
+	telegram-desktop \
+	virtualbox \
+	vlc \
+	wezterm \
+	wget \
+	zellij \
+	zsh \
+	cachyos/vscodium \
+
+# installing missing software via flatpak
+flatpak install -y \
+	org.gnome.World.PikaBackup \
+	com.logseq.Logseq \
+
+# TODO - install nekoray
+
+# setting up default shell
+chsh -s "$(which fish)"
+
+# setting up docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# solving inconsistencies with windows dual boot
+timedatectl set-local-rtc 1 --adjust-system-clock
