0e5e6f6fa5f56a7ce67ec0e94164efbba88464c5
Petr <petr@duck.com>
2024-08-22 09:56:37 +0300

    keybindings, repos and timefix added

--- scripts/extras/distro/garuda.sh
+++ scripts/extras/distro/garuda.sh
@@ -1,5 +1,31 @@
 #!/usr/bin/env bash
 
+# assuming gnome version
+# enabling ubuntu-like alt-tab and windows-like win-r
+echo Configuring Alt-tab, Super-grave and Super-tab...
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Shift><Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-group "['<Super>Above_Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-group-backward "['<Shift><Super>Above_Tab']"
+
+echo Configuring Win-r...
+gsettings set org.gnome.desktop.wm.keybindings panel-run-dialog "['<Alt>F1', '<Super>r']"
+
+echo Configuring scrolling...
+gsettings set org.gnome.desktop.peripherals.mouse natural-scroll false
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll true
+
+echo Configuring Super-Shift-s to make a screenshot...
+gsettings set org.gnome.shell.keybindings show-screenshot-ui "['Print', '<Shift><Super>s']"
+
+# enabling cachy os repos
+curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
+tar xvf cachyos-repo.tar.xz && cd cachyos-repo
+sudo ./cachyos-repo.sh
+
+# installing packages
 sudo pacman -Syu --noconfirm \
 	aria2 \
 	bash-language-server \
@@ -65,7 +91,12 @@ sudo pacman -Syu --noconfirm \
 	zellij \
 	zsh \
 
+# setting up default shell
 chsh -s "$(which fish)"
 
+# setting up docker
 sudo groupadd docker
 sudo usermod -aG docker $USER
+
+# solving inconsistencies with windows dual boot
+timedatectl set-local-rtc 1 --adjust-system-clock
