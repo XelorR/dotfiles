4cbc4a4059231266fd16461616ca82dfac1d38d1
Petr <petr@duck.com>
2024-08-30 20:10:49 +0300

    scripts cleanup

--- scripts/run_once_after_install-fish-plugins.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update && fish_config theme save "Catppuccin Mocha"'
