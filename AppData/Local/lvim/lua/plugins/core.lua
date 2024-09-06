075fcba06e195baf41e006953c01d94e77953271
Petr <petr@duck.com>
2024-09-06 16:11:36 +0300

    theme changed to darkest tokyonight

--- AppData/Local/lvim/lua/plugins/core.lua
+++ AppData/Local/lvim/lua/plugins/core.lua
@@ -2,8 +2,9 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
+      colorscheme = "tokyonight-night",
       -- colorscheme = "tokyonight",
-      colorscheme = "catppuccin",
+      -- colorscheme = "catppuccin",
     },
   },
 }
