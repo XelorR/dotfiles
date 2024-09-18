fef654f410e5b0dd97a3dbb6b9aa4e7aefe3b752
Petr <petr@duck.com>
2024-09-19 01:18:52 +0300

    hippie expand as default expand for em enabled

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -25,7 +25,7 @@ if status is-interactive
     alias note='$VISUAL $HOME/org/journals/$(date -d "0 days ago" +%Y_%m_%d).org'
 
     if command -v emacs &>/dev/null
-        alias em="emacs -nw -Q --eval '(progn (setq make-backup-files nil) (menu-bar-mode -1) (setq mode-line-format nil))'"
+        alias em="emacs -nw -Q --eval '(progn (setq make-backup-files nil) (menu-bar-mode -1) (setq mode-line-format nil) (global-set-key [remap dabbrev-expand] \'hippie-expand))'"
         alias macs="emacsclient -a '' -c -nw"
         alias orgnote='em $HOME/org/journals/$(date -d "0 days ago" +%Y_%m_%d).org'
         alias doomnote='macs $HOME/org/journals/$(date -d "0 days ago" +%Y_%m_%d).org'
